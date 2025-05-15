e28977de8f655beddeafb9b42a262545777eed56
이찬호 <chanho@woowahan.com>
2025-05-15 23:17:13 +0900

    인증 제공자 추가: AuthProvider 컴포넌트를 생성하고 RootLayout에 통합

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,12 @@
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import "@/global.css";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
+import AuthProvider from "@/provider/AuthProvider";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
+import React from "react";
 import "react-native-reanimated";
 
-import { useColorScheme } from "@/hooks/useColorScheme";
-
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
@@ -25,8 +18,8 @@ export default function RootLayout() {
 
   return (
     <GluestackUIProvider mode="light">
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        <Stack>
+      <AuthProvider>
+        <Stack initialRouteName="(auth)">
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="(auth)" options={{ headerShown: false }} />
           <Stack.Screen
@@ -35,8 +28,7 @@ export default function RootLayout() {
           />
           <Stack.Screen name="+not-found" />
         </Stack>
-        <StatusBar style="auto" />
-      </ThemeProvider>
+      </AuthProvider>
     </GluestackUIProvider>
   );
 }
---
+++ provider/AuthProvider.tsx
@@ -0,0 +1,33 @@
+import React, { createContext, useContext, useState } from "react";
+
+export type User = {
+  name: string;
+};
+export const AuthContext = createContext<{
+  user: User | null;
+  setUser: (user: User) => void;
+  isAuthenticated: boolean;
+  isLoading: boolean;
+}>({
+  user: null,
+  setUser: () => {},
+  isAuthenticated: false,
+  isLoading: true,
+});
+export const useAuth = () => useContext(AuthContext);
+
+export default function AuthProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [user, setUser] = useState<User | null>(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser, isAuthenticated, isLoading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
