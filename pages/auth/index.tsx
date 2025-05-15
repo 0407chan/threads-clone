36b279d81245991fc4a577f56cebc1ce6b97b433
이찬호 <chanho@woowahan.com>
2025-05-15 23:17:16 +0900

    사용자 정보 표시: Auth 컴포넌트에서 사용자 이름을 표시하도록 수정

--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -1,10 +1,19 @@
-import React from "react";
+import { useAuth } from "@/provider/AuthProvider";
+import React, { useEffect } from "react";
 import { SafeAreaView, Text } from "react-native";
 
 export default function Auth() {
+  const { user, setUser } = useAuth();
+
+  useEffect(() => {
+    setUser({
+      name: "John Doe",
+    });
+  }, []);
+
   return (
     <SafeAreaView>
-      <Text>auth</Text>
+      <Text>Hello, {user?.name}</Text>
     </SafeAreaView>
   );
 }
